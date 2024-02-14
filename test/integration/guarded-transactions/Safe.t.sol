e935afc68e79f8af62b3bace5e7b0a04aa0b2ade
Alec DiFederico <alecdifederico@gmail.com>
2024-02-14 11:43:27 -0500

    added check to see if the guard is active before allowing the transaction

--- test/integration/guarded-transactions/Safe.t.sol
+++ test/integration/guarded-transactions/Safe.t.sol
@@ -1,6 +1,9 @@
 // SPDX-License-Identifier: BUSL-1.1
 pragma solidity ^0.8.20;
 
+import {ERC721} from "@openzeppelin-contracts/token/ERC721/ERC721.sol";
+import {Actions} from "@src/Kernel.sol";
+
 import {Errors} from "@src/libraries/Errors.sol";
 
 import {BaseTest} from "@test/BaseTest.sol";
@@ -33,5 +36,60 @@ contract GuardedTransactions_Safe_Integration_Test is BaseTest {
         // assert the ETH was transferred
         assertEq(address(alice.safe).balance, 0 ether);
         assertEq(address(bob.safe).balance, 1 ether);
+    }
+
+    // Tests that the Guard Policy can be deactivated by the kernel, preventing
+    // any movement of assets from the safe until either the Guard Policy is
+    // activated, or the owner of the rental safe upgrades to a newer Guard Policy.
+    function test_Success_GuardCanBeDeactivated() public {
+        // mint token to the rental wallet
+        erc721s[0].mint(address(alice.safe), 5);
+
+        // assert that the token exists in the safe
+        assertEq(erc721s[0].ownerOf(5), address(alice.safe));
+
+        // create the `transferFrom(address,address,uint256)` calldata
+        bytes memory transferFromCalldata = abi.encodeWithSelector(
+            ERC721.transferFrom.selector,
+            address(alice.safe),
+            bob.addr,
+            5
+        );
+
+        // Sign the calldata
+        bytes memory signature = SafeUtils.signTransaction({
+            safe: address(alice.safe),
+            ownerPrivateKey: alice.privateKey,
+            to: address(erc721s[0]),
+            transaction: transferFromCalldata
+        });
+
+        // Disable the guard policy
+        vm.prank(deployer.addr);
+        kernel.executeAction(Actions.DeactivatePolicy, address(guard));
+
+        // Expect the transaction to revert because the guard is deactivated
+        SafeUtils.executeTransactionWithError({
+            safe: address(alice.safe),
+            to: address(erc721s[0]),
+            transaction: transferFromCalldata,
+            signature: signature,
+            expectedError: abi.encodeWithSelector(Errors.GuardPolicy_Deactivated.selector)
+        });
+
+        // Reactivate the guard policy
+        vm.prank(deployer.addr);
+        kernel.executeAction(Actions.ActivatePolicy, address(guard));
+
+        // Expect the transaction to execute successfully
+        SafeUtils.executeTransaction({
+            safe: address(alice.safe),
+            to: address(erc721s[0]),
+            transaction: transferFromCalldata,
+            signature: signature
+        });
+
+        // assert that the token was transferred
+        assertEq(erc721s[0].ownerOf(5), bob.addr);
     }
 }
